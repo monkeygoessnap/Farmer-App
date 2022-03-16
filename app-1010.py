8527d70832065ca2ebda6b05b17de50fd786c08a
johnwang <monkeygoessnap@gmail.com>
2022-03-16 18:26:46 +0800

    m

--- app-1010.py
+++ app-1010.py
@@ -4,16 +4,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/favicon.ico')
-def favicon():
-    return
-
 @app.route('/')
 def index():
     return 'TestTest'
 
 
-@app.route('home')
+@app.route('/home')
 def home():
     return "YourHome"
 
