17ffb99531259355d49fca5a53d1042288cd02c6
johnwang <monkeygoessnap@gmail.com>
2022-03-16 18:06:11 +0800

    init

--- app.py
+++ app.py
@@ -1,8 +1,22 @@
+import flask
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/favicon.ico')
+def favicon():
+    return
+
 @app.route('/')
 def index():
-    return 'Hello world'
+    return 'TestTest'
 
+
+@app.route('home')
+def home():
+    return "YourHome"
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
