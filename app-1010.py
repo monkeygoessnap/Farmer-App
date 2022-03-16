8706317e5293caccd5d64ac11dd1b9a00e5cd457
johnwang <monkeygoessnap@gmail.com>
2022-03-16 19:00:00 +0800

    m

--- app-1010.py
+++ app-1010.py
@@ -1,17 +1,29 @@
 import flask
-import os
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'TestTest'
+
+    return render_template("index.html")
+
+@app.route('/farm1', methods=['GET', 'POST'])
+def farm1():
+
+    if request.method == 'POST':
+        pass
+
+    return render_template("farm1.html")
 
 
-@app.route('/home')
-def home():
-    return "YourHome"
+@app.route('/farm2', methods=['GET', 'POST'])
+def farm2():
+
+    if request.method == 'POST':
+        pass
+    
+    return render_template("farm2.html")
 
 if __name__ == "__main__":
     app.run()
